9fa5690e5d89952ae3d54acecc6f95b62e611f88
FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2025-02-08 22:09:12 -0500

    Delete src directory

--- src/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-const authMiddleware = (req, res, next) => {
-    if (req.session.adminId) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
-};
-
-module.exports = authMiddleware;
